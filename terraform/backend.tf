2af5b9120a7f3b36a90409ce92e19b97052d1193
aamrdit <aamir.butt@outlook.com>
2026-01-18 02:01:23 +0000

    Add CI/CD with GitHub Actions, S3 backend, and updated scripts 13th attempt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "twin-terraform-state-246728976544"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-central-1"
     dynamodb_table = "twin-terraform-locks"
+    encrypt        = true
   }
 }
